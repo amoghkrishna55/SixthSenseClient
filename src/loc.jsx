e46436c5f4cbe3828082ac92a91b225c2ff751bc
Amogh Krishna <amoghkrishna55@gmail.com>
2024-04-13 13:54:46 +0530

    test

--- src/loc.jsx
+++ src/loc.jsx
@@ -170,29 +170,27 @@ const CreatePage = () => {
           Set Current
         </Button>
       </CurrentLocation>
+      <br />
       <div style={{ width: "100%" }}>
-      <iframe
-        width="100%"
-        height="600"
-        frameborder="0"
-        scrolling="no"
-        marginheight="0"
-        marginwidth="0"
-        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=31.2445781,75.7022453+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-      >
-        <a href="https://www.gps.ie/">gps trackers</a>
-      </iframe>
-    </div>
+        <iframe
+          width="100%"
+          height="600"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=31.2445781,75.7022453+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        >
+          <a href="https://www.gps.ie/">gps trackers</a>
+        </iframe>
+      </div>
     </Wrapper>
-    
-   
   );
 };
 
 export default CreatePage;
 
 const Wrapper = styled.div`
-  
   display: flex;
   flex-direction: column;
   align-items: center;
